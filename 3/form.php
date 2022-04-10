77669038fa3426c0aee97f3cce4120393b9ee290
DmitriiM0102 <91017076+DmitriiM0102@users.noreply.github.com>
2022-04-11 00:40:24 +0300

    Add files via upload

--- 3/form.php
+++ 3/form.php
@@ -8,50 +8,47 @@
     <div class="form">
         <form action="index.php" method="POST">
 
-        <label>Введите своё имя:<br>
-        <input name="name">
-        </label><br>
+        <label>Введите своё имя:</label><br>
+        <input name="name"><br>
 
-        <label>Введите свой адрес электронной почты (Email):<br>
-        <input name="email" type="email">
-        </label><br>
+        <label>Введите свой адрес электронной почты (Email):</label><br>
+        <input name="email" type="email"><br>
 
-        <label>Дата рождения:<br>
+        <label>Дата рождения:</label><br>
         <select name="year">
         <option value="god"> Выбрать год </option>
         <?php 
         for($i=1800;$i<=2022;$i++){
             printf("<option value=%d>%d</option>",$i,$i);
         } ?> </select>
-        </label><br>
+        <br>
 
-        Ваш пол:<br>
-        <label><input type="radio" name="g" value="m">Мужчина</label>
-        <label><input type="radio" name="g" value="w">Женщина</label><br>
+        <label>Ваш пол:</label><br>
+        <input type="radio" name="g" value="m">Мужчина
+        <input type="radio" name="g" value="w">Женщина<br>
 
-        Количество конечностей:<br>
-        <label><input type="radio" name="limb">0</label>
-        <label><input type="radio" name="limb">1</label>
-        <label><input type="radio" name="limb">2</label>
-        <label><input type="radio" name="limb">3</label>
-        <label><input type="radio" name="limb">4</label><br>
+        <label>Количество конечностей:</label><br>
+        <input type="radio" name="limb">0
+        <input type="radio" name="limb">1
+        <input type="radio" name="limb">2
+        <input type="radio" name="limb">3
+        <input type="radio" name="limb">4<br>
 
-        <label>Список сверхспособностей:<br>
+        <label>Список сверхспособностей:</label><br>
         <select name="sp[]" multiple="multiple">
             <option value="immortal">Бессмертие</option>
             <option value="ghost">Прохождение сквозь стены</option>
             <option value="levitation">Левитация</option>
         </select>
-        </label><br>
+        <br>
 
-        <label>Биография (о себе):<br>
+        <label>Биография (о себе):<label><br>
         <textarea name="bio"></textarea>
-        </label><br>
+        <br>
 
-        С контрактом ознакомлены?<br>
-        <label>
-            <div><input type="checkbox" name="check">С контрактом ознакомлен</div>
-        </label><br>
+        <label>С контрактом ознакомлены?</label><br>
+        <div><input type="checkbox" name="check">С контрактом ознакомлен</div>
+        <br>
 
         <input type="submit" value="Отправить">
 
